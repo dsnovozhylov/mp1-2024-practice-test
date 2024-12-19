ad7a4c53cb466d343fe766398bfc377302731560
dsnovozhylov <d9162589@gmail.com>
2024-12-19 10:13:40 +0300

    Update task3.c

--- 3824Б1ПР5/16 Новожилов Данила Сергеевич/task3.c
+++ 3824Б1ПР5/16 Новожилов Данила Сергеевич/task3.c
@@ -4,3 +4,38 @@ int task3(char *s)
 {
   return -1;
 }
+Решение:
+#include <stdio.h>
+#define MAX(A, i) (A[i] == A[(i)])
+#define MIN(A, i) (A[i] == A[N - 1])
+#define BETWEEN(A, x, y) (x <= A[y] && A[x] >= A[y])
+
+int task1(unsigned A[], size_t N)
+{
+    int max_index = -1;
+    int min_index = -1;
+    for (size_t i = 0; i < N; ++i) {
+        if (MAX(A, i)) {
+            if (max_index == -1) {
+                max_index = i;
+            }
+        }
+    }
+    for (size_t i = 0; i < N; ++i) {
+        if (MIN(A, i)) {
+            if (min_index == -1) {
+                min_index = i;
+            }
+        }
+    }
+    if (max_index == -1 || min_index == -1) {
+        return -1;
+    }
+    int count = 0;
+    for (size_t i = max_index; i <= min_index; ++i) {
+        if (BETWEEN(A, i, min_index)) {
+            ++count;
+        }
+    }
+    return count;
+}
