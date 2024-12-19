82f84b64a89077091669021e643c56d0bb2ec67b
dsnovozhylov <d9162589@gmail.com>
2024-12-19 10:15:34 +0300

    Update task2.c

--- 3824Б1ПР5/16 Новожилов Данила Сергеевич/task2.c
+++ 3824Б1ПР5/16 Новожилов Данила Сергеевич/task2.c
@@ -6,3 +6,20 @@ int task2(unsigned A[], size_t n)
 {
   return -1;
 }
+Решение:
+#include <stdio.h>
+int task2(int A[], int n)
+{
+    int series_count = 0;
+    for (int i = 0; i < n; ++i) {
+        if (A[i] == A[i+1]) {
+            series_count++;
+        }
+    }
+    return series_count;
+}
+    int result = task2(A, n);
+    printf("Количество серий в массиве: %d\n", result);
+
+    return 0;
+}
